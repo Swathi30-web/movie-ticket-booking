5b2af3ec3e347f2b1f6bbc0bd8dad57211182324
Swathi <swathisudar623@gmail.com>
2026-09-03 16:40:37 +0530

    Fix Node types for Render

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://YOUR-RENDER-URL.onrender.com",
+  baseURL: " https://movie-ticket-booking-4-e0wg.onrender.com",
 });
 
 // =========================
